9d260ee9134e6a4bf757d2dda20b529e822d30f7
Yurij Petrov <yurpetr@gmail.com>
2021-12-25 14:18:25 +0200

    Remember Me

--- src/main/java/com/yurpetr/nps/NpsApplication.java
+++ src/main/java/com/yurpetr/nps/NpsApplication.java
@@ -8,29 +8,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class NpsApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(NpsApplication.class);
-	}
+   @Override
+   protected SpringApplicationBuilder configure(
+         SpringApplicationBuilder application) {
+      return application.sources(NpsApplication.class);
+   }
 
-	public static void main(String[] args) {
-		SpringApplication.run(NpsApplication.class, args);
+   public static void main(String[] args) {
+      SpringApplication.run(NpsApplication.class, args);
 
-	}
+   }
 
-	/**
-	 * Start internal H2 server so we can query the DB from IDE
-	 *
-	 * @return H2 Server instance
-	 * @throws SQLException
-	 */
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	public Server h2Server() throws SQLException {
-		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
-	}
+   /**
+    * Start internal H2 server so we can query the DB from IDE
+    *
+    * @return H2 Server instance
+    * @throws SQLException
+    */
+   @Bean(initMethod = "start", destroyMethod = "stop")
+   public Server h2Server() throws SQLException {
+      return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort",
+            "9092");
+   }
+
+   @Bean
+   public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+   }
 
 }
--- src/main/java/com/yurpetr/nps/config/WebSecurityConfig.java
+++ src/main/java/com/yurpetr/nps/config/WebSecurityConfig.java
@@ -4,18 +4,21 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.yurpetr.nps.service.UserService;
+import com.yurpetr.nps.repo.UsersRepository;
+import com.yurpetr.nps.service.UserServiceImpl;
 
 @Configuration
 @EnableWebSecurity
@@ -34,51 +37,67 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
          "/logoutpage",
          "/password_reset"
    };
-   
-   private UserService userService;
-   
+
    @Autowired
-   public void setUserService(UserService userService) {
-	this.userService = userService;
+   private UserDetailsService userService;
+
+   public void setUserService(UserServiceImpl userService) {
+      this.userService = userService;
    }
 
-   @Autowired
-   DataSource dataSource;
-
-    @Override protected void configure(HttpSecurity http) throws Exception { 
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
       http
-      .authorizeRequests()
-      .antMatchers("/h2/**").permitAll()
-      .antMatchers(HttpMethod.GET, GET_PUBLIC_URLS).permitAll()
-      .antMatchers(HttpMethod.POST, POST_PUBLIC_URLS).permitAll()
-      .antMatchers("/admin**").hasAnyRole("ADMIN")
-      .anyRequest().authenticated()
-      .and()
-      .formLogin().loginPage("/login").permitAll()
-      .defaultSuccessUrl("/")
-      .failureUrl("/login?error=true")
-      .and()
-      .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
-      .logoutSuccessUrl("/")
-//      .and()
-//      .rememberMe()
-      ; 
+            .authorizeRequests()
+            .antMatchers("/h2/**").permitAll()
+            .antMatchers(HttpMethod.GET, GET_PUBLIC_URLS).permitAll()
+            .antMatchers(HttpMethod.POST, POST_PUBLIC_URLS).permitAll()
+            .antMatchers("/admin**").hasAnyRole("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/login").permitAll()
+            .defaultSuccessUrl("/")
+            .failureUrl("/login?error=true")
+            .and()
+            .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .permitAll()
+            .logoutSuccessUrl("/")
+            .and()
+            .rememberMe().tokenRepository(persistentTokenRepository());
       http.csrf().ignoringAntMatchers("/h2/**");
       http.headers().frameOptions().sameOrigin();
-      
-   }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
-    
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-    	DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-    	authenticationProvider.setPasswordEncoder(passwordEncoder());
-    	authenticationProvider.setUserDetailsService(userService);
-    	return authenticationProvider;
-    }
+   }
+
+   @Autowired
+   public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+   }
+
+   @Bean
+   public DaoAuthenticationProvider daoAuthenticationProvider() {
+      DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+      authenticationProvider.setPasswordEncoder(passwordEncoder());
+      authenticationProvider.setUserDetailsService(userService);
+      return authenticationProvider;
+   }
+
+   @Autowired
+   private DataSource dataSource;
+
+   @Bean
+   public PersistentTokenRepository persistentTokenRepository() {
+      JdbcTokenRepositoryImpl tokenRepo = new JdbcTokenRepositoryImpl();
+      tokenRepo.setDataSource(dataSource);
+      return tokenRepo;
+   }
+
+   @Autowired
+   private UsersRepository usersRepository;
+
+   @Bean
+   public UserDetailsService userDetailsService() {
+      return new UserServiceImpl(usersRepository);
+   }
+
 }
