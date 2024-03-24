7757506279fde7fdf6f85a685aee5f16d0e5cd37
Yurii Petrov <yurpetr@gmail.com>
2024-03-24 21:17:50 +0200

    import fix

--- src/main/java/com/yurpetr/nps/config/WebSecurityConfig.java
+++ src/main/java/com/yurpetr/nps/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
