228494e4fd51d2b18d2f24bc2f4efe1059d9ae28
Yurii Petrov <yurpetr@gmail.com>
2024-04-18 21:29:45 +0300

    attempt of request limit integration

--- src/main/java/com/yurpetr/nps/config/WebSecurityConfig.java
+++ src/main/java/com/yurpetr/nps/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.yurpetr.nps.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
