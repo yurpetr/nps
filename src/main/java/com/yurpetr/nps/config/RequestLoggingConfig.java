a7ee3c18ebc25ccbe3c9becfaf276aa2ef26ce26
Yurii Petrov <yurpetr@gmail.com>
2024-05-19 11:22:57 +0300

    add logging requests

---
+++ src/main/java/com/yurpetr/nps/config/RequestLoggingConfig.java
@@ -0,0 +1,24 @@
+package com.yurpetr.nps.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+//@Configuration
+public class RequestLoggingConfig {
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(false);
+
+		return loggingFilter;
+
+	}
+
+}
